3f4dc23d5d0a27c543cabb1a9a7d1ba23596f617
valentineejk <valentineejk@gmail.com>
2022-08-13 14:11:37 +0100

    Initial Commit

---
+++ todo-app/todo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_appApp.swift
+//  todo-app
+//
+//  Created by Valentineejk on 13/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct todo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
